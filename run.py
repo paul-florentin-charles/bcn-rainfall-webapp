e2ac8fb02b20707923a46e399c4b5c4df3a00cc3
polochinoc <paul.charles@tutanota.com>
2025-06-26 18:32:03 +0200

    Update package bcn-rainfall-api-client to 1.0.9 and add API health check before running Webapp

--- run.py
+++ run.py
@@ -3,12 +3,23 @@
 """
 Script to run Flask server.
 """
+import sys
 
+from requests.exceptions import ConnectionError
+
+from bcn_rainfall_webapp import api_client
 from bcn_rainfall_webapp.app import flask_app
 from bcn_rainfall_webapp.config import Config
 
 
 def run():
+    # 1. Make sure API is running
+    try:
+        api_client.head_api('/', timeout=3)
+    except ConnectionError:
+        sys.exit(f"Cannot connect to API at '{api_client.base_url}', make sure it is running.")
+
+    # 2. Run Webapp
     flask_app.run(**Config().get_webapp_server_settings.model_dump())
 
 
