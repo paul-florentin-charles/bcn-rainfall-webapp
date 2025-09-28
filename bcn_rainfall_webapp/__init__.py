f3244864ad57322c523c34d6ed3172a40ffba5e1
polochinoc <paul.charles@tutanota.com>
2025-09-28 13:29:00 +0200

    Flush all Redis DB after init of client

--- bcn_rainfall_webapp/__init__.py
+++ bcn_rainfall_webapp/__init__.py
@@ -1,6 +1,7 @@
 from bcn_rainfall_webapp.db import DBClient
 
 db_client = DBClient.from_config()
+db_client.client.flushall()
 
 # TODO: these variables should not be fixed but set up dynamically from UI
 NORMAL_YEAR = 1981
